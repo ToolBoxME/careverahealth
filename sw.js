95995bb1daa9a09b07efa5022fa8ce5580318a86
abdallahabdullah2002 <abdallahabdullah2002@gmail.com>
2026-06-07 17:27:27 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
     "zoneId": 11114158
-}self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 11114313
 }
-self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
-
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
